e6835b4e6d737da93c17cab022e2b23cc906063d
steveoro <steve.alloro@gmail.com>
2015-12-07 20:52:36 +0100

    misc generator clean up

--- lib/generators/install/install_generator.rb
+++ lib/generators/install/install_generator.rb
@@ -1,9 +1,12 @@
+require 'rails/generators/base'
+
+
 module ScadaDb
   module Generators
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
 
-      def copy_all_db_template_files
+      def copy_db_files
         source_dir = File.expand_path("../templates", __FILE__)
         directory source_dir, "db"
       end
