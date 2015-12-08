574601d76fef24d73fee2544f04eb35d84f596a7
steveoro <steve.alloro@gmail.com>
2015-12-08 02:23:29 +0100

    improved install generator

--- lib/generators/scada_db/install/install_generator.rb
+++ lib/generators/scada_db/install/install_generator.rb
@@ -12,21 +12,37 @@ module ScadaDb
   - version:  0.0.3
   - author:   Steve A.
 
-Commodity generator that allows to have a base DB structure copied
-directly under the /db folder.
+This install generator will:
+
+  1. prepare the base folder structure for the DB management; typically:
+        db/diff.applied
+        db/diff.new
+        db/dump
+        db/seed
+
+  2. copy the latest available (committed) DB structure dump
+     into the dedicated folder:
+        db/dump/development.sql.bz2
+
+  3. copy any available factory into the mounting app
+     spec/factories folder
+
+  4. copy any available spec/support file into the mounting app
+     spec/support folder
+
 
 The db/dump folder contains the structure dumps that can be recreated
 easily with:
 
   > zeus rake db:rebuild_from_dump # (when using Zeus)
-
   ...Or...
-
   > bundle exec rake db:rebuild_from_dump
 
 The additional db/diff.new and db/diff.applied folders can be used
 to store SQL diff files to be applied to the dump using the dedicated
 rake task db:diff_apply
+
+Check out the corresponding rake task descriptions for more info.
 
 
 === Usage:
@@ -40,6 +56,16 @@ rake task db:diff_apply
       def copy_db_files
         source_dir = File.expand_path("../templates", __FILE__)
         directory source_dir, "db"
+      end
+
+      def copy_factories
+        source_dir = File.expand_path("../../../../../spec/factories", __FILE__)
+        directory source_dir, "spec/factories"
+      end
+
+      def copy_support
+        source_dir = File.expand_path("../../../../../spec/support", __FILE__)
+        directory source_dir, "spec/support"
       end
       #-- ---------------------------------------------------------------------
       #++
